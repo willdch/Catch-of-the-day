521f463d8057293df4a9194966e3fd3c78ab24ac
William Harris <william.harris@Will-Harris-Macbook-Air.local>
2019-01-19 23:25:50 -0800

    part 3 - 6

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>Catch of the Day</h1>
+        <h3 className="tagLine">
+          <span>Fresh Daily</span>
+        </h3>
+      </header>
+    );
+  }
+}
+
+export default Header;
