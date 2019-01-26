4221ed55851efe20dfb54f6254fba2517b99a696
William Harris <william.harris@Will-Harris-Macbook-Air.local>
2019-01-25 23:36:11 -0800

    all about state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { getFunName } from '../helpers';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+
+  goToStore= event => {
+    // 1. stop the form from submitting by defualt
+    event.preventDefault();
+  // 2. get text form that input
+const storeName = this.myInput.current.value;
+  // 3. change the page to /store/what-ever-they-entered
+  this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={ getFunName() }/>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store ➜ </button>
         </form>
-      </React.Fragment>
     );
   }
 }
